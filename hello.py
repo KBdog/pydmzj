0553eb0c4540bc65eef9d9aa60671f7e5b06ea1e
Zhang Haodong <savvy.chn@gmail.com>
2019-10-21 15:30:13 +0800

    Initial commit

---
+++ hello.py
@@ -0,0 +1,21 @@
+import urllib.request
+import json
+import jsonpath
+
+def get_record(url):
+    resp = urllib.request.urlopen(url)
+    ele_json = json.loads(resp.read())
+    return ele_json
+
+if __name__ == '__main__':
+    a = int(input("输入一个漫画id下限："))
+    x = a
+    b = int(input("输入一个漫画id上限: "))
+    while x <= b :
+        url = 'http://v2.api.dmzj.com/novel/%d.json' % (x)
+        id = jsonpath.jsonpath(get_record(url),'$.id')
+        print('id:', id)
+        name = jsonpath.jsonpath(get_record(url),'$.name')
+        print('name:', name)
+        #print(get_record(url))
+        x += 1
