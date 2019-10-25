4fbb72ec9a827e14e0caa4bcf49c8217414a6042
Zhang Haodong <savvy.chn@gmail.com>
2019-10-25 15:01:35 +0800

    改用字典

--- ComicID.py
+++ ComicID.py
@@ -2,7 +2,6 @@
 
 import urllib.request
 import json
-import jsonpath
 
 #获取json的函数：
 def get_record(url):
@@ -16,11 +15,23 @@ if __name__ == '__main__':
     b = int(input("输入一个漫画id上限: "))
     while x <= b :
         url = 'http://v2.api.dmzj.com/comic/%d.json' % (x)
+        '''
+            改用字典操作
+        '''
+        comic = get_record(url)
+        if type(comic) == dict:
+            print('id:',comic['id'])
+            print('title:',comic['title'])
+            print('author:',comic['authors'][0]['tag_name'])
+        x += 1
+
+    '''
         id = jsonpath.jsonpath(get_record(url),'$.id')
         if(id == False):
             x += 1
         else:
             print('id:', id[0])
-            title = jsonpath.jsonpath(get_record(url),'$.title')
-            print('name:', title[0])
+            title = jsonpath.jsonpath(get_record(url),'$.tag_name')
+            print('name:', title)
             x += 1
+    '''
